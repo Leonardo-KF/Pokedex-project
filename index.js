8b59de52bc1bb1649c0630c67729e607664dc67c
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-09-29 21:52:06 -0300

    update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const lista = ["teste1", "teste2", "teste3", "teste4", "teste5"];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
     const lista = ["teste1", "teste2", "teste3", "teste4", "teste5"];
     res.render('index', {titulo:"Pagina principal", conteudos: lista});
@@ -14,6 +16,16 @@ app.get('/cadastro', (req, res) => {
 }) 
 app.get('/detalhes', (req, res) => {
     res.render('detalhes');
+})
+app.get('/formulario', (req, res) => {
+    const {nome, email, password} = req.body;
+    res.send({nome: nome, email: email, password: password});
+})
+app.post('/formulario', (req, res) => {
+    const {nome, email, password} = req.body;
+    const site = {nome: nome, email: email, password: password}
+    res.render('index')
+    res.send({nome: nome, email: email, password: password});
 })
 
 app.listen(3000);
