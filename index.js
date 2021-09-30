e783c2789bb36926e7e74c07929fb613cbe753ea
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-09-29 22:46:00 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const lista = ["teste1", "teste2", "teste3", "teste4", "teste5"];
+const lista = [{},{},{}];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
