23135270c5e62bce1db3cb3d1dccd5e2736d0913
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-01 23:59:29 -0300

    update

--- index.js
+++ index.js
@@ -3,40 +3,40 @@ const app = express();
 const path = require("path");
 const Pokemons = [
   {
-    numero: 004,
-    nome: "Charmander",
-    tipo: "Fogo",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-    descricao:
+    Numero: 004,
+    Nome: "Charmander",
+    Tipo: "Fogo",
+    Imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    Descricao:
       "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta da cauda.",
-    altura: 0.6,
-    peso: 8.5,
-    categoria: "Lagarto",
-    habilidade: "Chama",
+    Altura: 0.6,
+    Peso: 8.5,
+    Categoria: "Lagarto",
+    Habilidade: "Chama",
   },
   {
-    numero: 001,
-    nome: "Bulbasaur",
-    tipo: "Grama",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-    descricao:
+    Numero: 001,
+    Nome: "Bulbasaur",
+    Tipo: "Grama",
+    Imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    Descricao:
       "Há uma semente de planta em suas costas desde o dia em que o Pokémon nasceu. A semente cresce lentamente.",
-    altura: 0.7,
-    peso: 6.9,
-    categoria: "Semente",
-    habilidade: "Crescimento Exagerado",
+    Altura: 0.7,
+    Peso: 6.9,
+    Categoria: "Semente",
+    Habilidade: "Crescimento Exagerado",
   },
   {
-    numero: 007,
-    nome: "Squirtle",
-    tipo: "Agua",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-    descricao:
+    Numero: 007,
+    Nome: "Squirtle",
+    Tipo: "Agua",
+    Imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    Descricao:
       "Quando ele retrai seu longo pescoço em sua concha, ele esguicha água com força vigorosa.",
-    altura: 0.5,
-    peso: 9.0,
-    categoria: "Pequena Tartaruga",
-    habilidade: "Torrente",
+    Altura: 0.5,
+    Peso: 9.0,
+    Categoria: "Pequena Tartaruga",
+    Habilidade: "Torrente",
   },
 ];
 
@@ -57,7 +57,6 @@ app.get("/detalhes/:ind", (req, res) => {
   const indice = req.params.ind;
   const pokemons = Pokemons[indice];
   res.render("detalhes", { pokemon: pokemons });
-  console.log(pokemons);
 });
 
 app.get("/formulario", (req, res) => {
